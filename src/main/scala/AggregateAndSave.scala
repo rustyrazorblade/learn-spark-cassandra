2842a5aafc4c70deb82319f1cad0ed960f4e7f1c
Jon Haddad <jon@jonhaddad.com>
2014-09-18 13:38:46 -0700

    comments

--- src/main/scala/AggregateAndSave.scala
+++ src/main/scala/AggregateAndSave.scala
@@ -20,7 +20,15 @@
   insert into demo (id, v) VALUES ( 4, 4);
 
   create table stats ( k timeuuid primary key, total int );
-  select dateOf(k), total from stats ;
+
+
+  After you run the job, select from the table:
+
+ cqlsh:tutorial> select dateOf(k), total from stats ;
+
+ dateOf(k)                | total
+--------------------------+-------
+ 2014-09-18 13:22:37-0700 |   390
 
  */
 
@@ -33,18 +41,32 @@ import com.datastax.spark.connector._
 object AggregateAndSave {
   def main(args: Array[String]): Unit = {
 
+    // the setMaster("local") lets us run & test the job right in our IDE
     val conf = new SparkConf(true).set("spark.cassandra.connection.host", "127.0.0.1").setMaster("local")
 
+    // "local" here is the master, meaning we don't explicitly have a spark master set up
     val sc = new SparkContext("local", "test", conf)
+
+    // we need this later to execute queries
+    // there's another way of writing data to cassandra, we'll cover it later
+    // this is good for single queries
+    // the future version will be for saving RDDs
     val connector = CassandraConnector(conf)
 
+    // keyspace & table
     val rdd = sc.cassandraTable("tutorial", "demo")
+
+    // get a simple count
     val result = rdd.count()
 
+    // map accepts a function.  the function receives a CassandraRow
+    // we return a new result for each row.  in this case, our "v" column
     val kvals = rdd.map(s => s.getInt("v"))
 
+    // we use reduce to sum all the "v" columns we extracted in the previous map function
     val ksum : java.lang.Integer = kvals.reduce((a,b) => a + b)
 
+    // we can get a session out of the connector to insert our data back into cassandra
     connector.withSessionDo(session => {
       val prepared = session.prepare("INSERT INTO tutorial.stats (k, total) VALUES (now(), ?)")
       val bound = prepared.bind(ksum)
@@ -53,8 +75,6 @@ object AggregateAndSave {
 
     println("result", result)
     println("ksum", ksum)
-
-
 
   }
 
